a2e30c9ff24203fc3aab78c63aad2320fef789f5
Rahil Shaikh <rahilz.shaikh17@gmail.com>
2024-05-06 13:05:23 +0530

    first commit

---
+++ python.py
@@ -0,0 +1,7 @@
+# This is just a python code for tesing purpose as i am pushing, pulling, stashing, commiting the code. and its just for practice purpose.
+
+a = []
+b= ['Rahil', 'Shaikh']
+for i  in b:
+    a.append(i)
+    print(a)
